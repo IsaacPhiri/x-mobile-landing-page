affec0b995702886d9182a5e9ae9d10d51491054
Isaac Phiri <isaacphiri315@gmail.com>
2025-01-29 18:10:53 +0200

    refactor App component to use functional state and update UI with a welcome message and button interaction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [ message, setMessage ] = useState('Welcome to X Mobile Landing Page');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          X Mobile Landing Page!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{message}</h1>
+      <button onClick={ () => setMessage('You clicked me!') }>
+        Click me
+      </button>
     </div>
   );
 }
